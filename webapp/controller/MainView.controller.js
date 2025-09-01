f4d89d95f4439d0859ab086b5d891b17fd64120b
Shahabuddin Ansari <ansari.extc@gmail.com>
2025-09-01 22:18:14 +0530

    Added Expandable Panel

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,138 +1,197 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/m/StandardListItem",
-    "sap/ui/model/json/JSONModel"
-], (Controller, Filter, FilterOperator, StandardListItem, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+  ],
+  (Controller, Filter, FilterOperator, StandardListItem, JSONModel) => {
     "use strict";
 
     return Controller.extend("todolist.hds.ui5.todolist.controller.MainView", {
-        onInit() {
-            console.log("MainView controller initialized");
-            this.bindFilteredList();
-        },
+      onInit() {
+        console.log("MainView controller initialized");
+        this.bindFilteredList();
+      },
 
-        bindFilteredList(){
-            //Bind pending tasks
-            this.byId("taskList_pending").bindItems({
-                path: "/tasks",
-                filters: [new Filter("isCompleted", FilterOperator.EQ, false)],
-                template: new StandardListItem({
-                    title: "{title}"
-                })
-            });
+      bindFilteredList() {
+        //Bind pending tasks
+        this.byId("taskList_pending").bindItems({
+          path: "/tasks",
+          filters: [new Filter("isCompleted", FilterOperator.EQ, false)],
+          template: new StandardListItem({
+            title: "{title}",
+          }),
+        });
 
-            //Bind completed tasks
-            this.byId("taskList_completed").bindItems({
-                path: "/tasks",
-                filters: [new Filter("isCompleted", FilterOperator.EQ, true)],
-                template: new StandardListItem({
-                    title: "{title}",
-                    highlight: "Success"
-                })
-            })
-        },
+        //Bind completed tasks
+        this.byId("taskList_completed").bindItems({
+          path: "/tasks",
+          filters: [new Filter("isCompleted", FilterOperator.EQ, true)],
+          template: new StandardListItem({
+            title: "{title}",
+            highlight: "Success",
+          }),
+        });
+      },
 
-        submitTaskHandler() {
-            this.addTaskHandler();
-        },
+      submitTaskHandler() {
+        this.addTaskHandler();
+      },
 
-        addTaskHandler() {
-            // Read input value
-            const oInput = this.byId("inputTask")
-            const sInputValue = oInput.getValue().trim();
+      addTaskHandler() {
+        // Read input value
+        const oInput = this.byId("inputTask");
+        const sInputValue = oInput.getValue().trim();
 
-            // reset the input field
-            oInput.setValue("");
+        // reset the input field
+        oInput.setValue("");
 
-            // Check if valid inputs
-            if (!sInputValue.trim()) return
+        // Check if valid inputs
+        if (!sInputValue.trim()) return;
 
-            // update the input to the list of task
-            const aTasks = this.getView().getModel().getProperty("/tasks");
-            aTasks.push({
-                id: Date.now().toString(),
-                title: sInputValue,
-                isCompleted: false
-            });
+        // update the input to the list of task
+        const aTasks = this.getView().getModel().getProperty("/tasks");
+        aTasks.push({
+          id: Date.now().toString(),
+          title: sInputValue,
+          isCompleted: false,
+        });
 
-            this.getView().getModel().setProperty("/tasks", aTasks);
+        this.getView().getModel().setProperty("/tasks", aTasks);
+        this.updatePendingTaskPanelVisibility();
+        this.updateCompletedTaskPanelVisibility();
+      },
 
-            console.log(aTasks)
-        },
-        selectTaskHandler(oEvent) {
-            const oSelectedRow = oEvent?.getSource()?.getSelectedItem()?.getBindingContext()?.getObject();
+      updatePendingTaskPanelVisibility() {
+        const oPanelPendingTask = this.byId("panel__pendingTasks");
+        const aPendingTask = this.getView()
+          .getModel()
+          .getProperty("/tasks")
+          ?.filter((tasks) => !tasks.isCompleted).length;
 
-            // Enable Delete / MarkComplete buttona
-            if (oSelectedRow) this.byId("deleteTaskBtn").setEnabled(true);
-            if (oSelectedRow && !oSelectedRow.isCompleted) this.byId("completeTaskBtn").setEnabled(true);
+        oPanelPendingTask.setExpanded(aPendingTask > 0);
+        oPanelPendingTask.setHeight(aPendingTask > 10 ? "350px" : "auto");
+        oPanelPendingTask.setHeaderText(`View pending tasks (${aPendingTask})`);
+      },
 
-            if (oSelectedRow) {
-                this.enabledCancelButton()
-            } else this.disableCancelButton();
+      updateCompletedTaskPanelVisibility() {
+        const oPanelCompletedTask = this.byId("panel__completedTasks");
+        const aCompletdTask = this.getView()
+          .getModel()
+          .getProperty("/tasks")
+          ?.filter((tasks) => tasks.isCompleted).length;
 
-            if (oSelectedRow && !oSelectedRow.isCompleted) { this.enabledMarkCompleteButton() } else this.disableMarkCompleteButton();
+        // oPanelCompletedTask.setExpanded(aCompletdTask > 0);
+        oPanelCompletedTask.setHeight(aCompletdTask > 10 ? "350px" : "auto");
+        oPanelCompletedTask.setHeaderText(
+          `View completed tasks (${aCompletdTask})`
+        );
+      },
+      selectTaskHandler(oEvent) {
+        const oSelectedRow = oEvent
+          ?.getSource()
+          ?.getSelectedItem()
+          ?.getBindingContext()
+          ?.getObject();
 
-        },
+        // Enable Delete / MarkComplete buttona
+        if (oSelectedRow) this.byId("deleteTaskBtn").setEnabled(true);
+        if (oSelectedRow && !oSelectedRow.isCompleted)
+          this.byId("completeTaskBtn").setEnabled(true);
 
-        enabledCancelButton() {
-            this.byId("deleteTaskBtn").setEnabled(true);
-        },
+        if (oSelectedRow) {
+          this.enabledCancelButton();
+        } else this.disableCancelButton();
 
-        enabledMarkCompleteButton() {
-            this.byId("completeTaskBtn").setEnabled(true);
-        },
+        if (oSelectedRow && !oSelectedRow.isCompleted) {
+          this.enabledMarkCompleteButton();
+        } else this.disableMarkCompleteButton();
+      },
 
-        disableCancelButton() {
-            this.byId("deleteTaskBtn").setEnabled(false);
-        },
+      enabledCancelButton() {
+        this.byId("deleteTaskBtn").setEnabled(true);
+      },
 
-        disableMarkCompleteButton() {
-            this.byId("completeTaskBtn").setEnabled(false);
-        },
+      enabledMarkCompleteButton() {
+        this.byId("completeTaskBtn").setEnabled(true);
+      },
 
-        markCompleteTaskHandler() {
-            const oList = this.byId("taskList_pending");
-            const oModel = this.getView().getModel();
+      disableCancelButton() {
+        this.byId("deleteTaskBtn").setEnabled(false);
+      },
 
-            const oSelectedRow = oList?.getSelectedItem()?.getBindingContext()?.getObject();
-            if (!oSelectedRow?.id) return;
+      disableMarkCompleteButton() {
+        this.byId("completeTaskBtn").setEnabled(false);
+      },
 
-            // oSelectedRow.isCompleted = true;
-            // oModel.refresh();
+      markCompleteTaskHandler() {
+        const oList = this.byId("taskList_pending");
+        const oModel = this.getView().getModel();
 
-            const aTasks = this.getView().getModel().getProperty("/tasks");
-            const aUpdatedTasks = aTasks.map(task => {
-                if(task.id === oSelectedRow.id) {
-                    return {
-                        ...task,
-                        'isCompleted': true
-                    }
-                } else return task
-            })
+        const oSelectedRow = oList
+          ?.getSelectedItem()
+          ?.getBindingContext()
+          ?.getObject();
+        if (!oSelectedRow?.id) return;
 
-            this.getView().getModel().setProperty("/tasks", aUpdatedTasks);
+        // oSelectedRow.isCompleted = true;
+        // oModel.refresh();
 
-            this.disableCancelButton();
-            this.disableMarkCompleteButton();
-        },
+        const aTasks = this.getView().getModel().getProperty("/tasks");
+        const aUpdatedTasks = aTasks.map((task) => {
+          if (task.id === oSelectedRow.id) {
+            return {
+              ...task,
+              isCompleted: true,
+            };
+          } else return task;
+        });
 
-        deleteTaskHandler() {
-            const sSelectedTaskId = this.byId("taskList_pending")?.getSelectedItem()?.getBindingContext()?.getObject()?.id;
+        this.getView().getModel().setProperty("/tasks", aUpdatedTasks);
 
-            if (!sSelectedTaskId) return;
+        this.disableCancelButton();
+        this.disableMarkCompleteButton();
 
-            const aTasks = this.getView().getModel().getProperty("/tasks");
+        this.updatePendingTaskPanelVisibility();
+        this.updateCompletedTaskPanelVisibility();
+      },
 
-            const aUpdatedTasks = aTasks.filter(task => task.id !== sSelectedTaskId);
-            this.getView().getModel().setProperty("/tasks", aUpdatedTasks);
+      deleteTaskHandler() {
+        const sSelectedTaskId = this.byId("taskList_pending")
+          ?.getSelectedItem()
+          ?.getBindingContext()
+          ?.getObject()?.id;
 
-            this.byId("taskList_pending").removeSelections(true);
+        if (!sSelectedTaskId) return;
 
-            // Disabled the delete button
-            this.disableCancelButton();
-            this.disableMarkCompleteButton();
+        const aTasks = this.getView().getModel().getProperty("/tasks");
+
+        const aUpdatedTasks = aTasks.filter(
+          (task) => task.id !== sSelectedTaskId
+        );
+        this.getView().getModel().setProperty("/tasks", aUpdatedTasks);
+
+        this.byId("taskList_pending").removeSelections(true);
+
+        // Disabled the delete button
+        this.disableCancelButton();
+        this.disableMarkCompleteButton();
+
+        // Update visibility of Panels
+        this.updatePendingTaskPanelVisibility();
+        this.updateCompletedTaskPanelVisibility();
+      },
+
+      expandPendingPanelHandler(oEvent) {
+        const isPanelExpanded = oEvent.getParameter("expand");
+        const oPanelPendingTask = this.byId("panel__pendingTasks");
+
+        if (!isPanelExpanded) {
+          oPanelPendingTask.setHeight("auto");
         }
+      },
     });
-});
+  }
+);
