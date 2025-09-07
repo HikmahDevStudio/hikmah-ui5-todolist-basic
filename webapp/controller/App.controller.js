e13c74f47fe4433ade0dca2ed8f55f265c93be07
HikmahDevStudio <hikmahdevstudio@gmail.com>
2025-09-07 15:28:43 +0530

    Updated directory

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,7 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+sap.ui.define(["sap/ui/core/mvc/Controller"], (BaseController) => {
   "use strict";
 
-  return BaseController.extend("todolist.hds.ui5.todolist.controller.App", {
-      onInit() {
-      }
+  return BaseController.extend("hds.ui5.todolist.controller.App", {
+    onInit() {},
   });
 });
